6a9f56995ae6908036ba4f983555ec15b790b317
Jeronimo Deli Larios <jeronimo.deli@gmail.com>
2024-11-26 14:19:22 -0600

    cambiar link

--- main_scraping.py
+++ main_scraping.py
@@ -4,7 +4,7 @@ from extract_links_calendar import extract_event_links_from_calendar
 from url_imagen import download_image
 def main():
     # URL to scrape event links
-    main_url = "http://boletin.itam.mx/mail/repertorio/2024/47/index.html"
+    main_url = "http://boletin.itam.mx/mail/repertorio/2024/48/index.html"
 
     # Step 1: Extract all event links
     event_links = extract_event_links_from_calendar(main_url)
@@ -12,7 +12,7 @@ def main():
     # Step 2: Extract title and description for each event link
     for link in event_links:
         title, description, image = extract_title_description_and_image(link)
-        download_image(image)
+        #download_image(image)
         # Check if the image contains "logo-ITAM" and skip if true
         if "logo-ITAM" in image:
             continue
